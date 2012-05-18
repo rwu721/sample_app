b5190d1481db153ad0247a147685b7e1a3c53175
Ray Wu <rwu721@gmail.com>
2012-05-17 17:46:27 -0700

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,11 @@ SampleApp::Application.configure do
 
   # Enable threaded mode
   # config.threadsafe!
-
+  
+  # Force all access to the app over SSL, use Strict-Transport-Security, 
+   # and use secure cookies.
+   config.force_ssl = true
+   
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
