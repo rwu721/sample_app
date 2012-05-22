c5fd74351476387d374f743c2bfeafeaa3674cc3
Ray Wu <rwu721@gmail.com>
2012-05-22 01:42:45 -0700

    Finish sign in - signout not workign

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -26,12 +26,18 @@ describe "Authentication" do
             fill_in "Password", with: user.password
             click_button "Sign in"
           end
+          
 
           it { should have_selector('title', text: user.name) }
           it { should have_link('Profile', href: user_path(user)) }
           it { should have_link('Sign out', href: signout_path) }
           it { should_not have_link('Sign in', href: signin_path) }
         end
-      end
+        describe "followed by signout" do
+                before { click_link "Sign out" }
+                it { should have_link('Sign in') }
+              end
+            end
+      
     
   end
