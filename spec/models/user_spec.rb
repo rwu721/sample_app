e4de3a0ab2c90ffb8f40ab14607fe3e5cc43f829
Ray Wu <rwu721@gmail.com>
2012-05-22 01:17:37 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,7 +25,9 @@ describe User do
   it { should respond_to(:password_digest)}
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+  
   it { should be_valid }
 
   describe "when name is not present" do
@@ -102,5 +104,9 @@ describe User do
       before { @user.password = @user.password_confirmation = "a" * 5 }
       it { should be_invalid }
     end
+    describe "remember token" do
+       before { @user.save }
+       its(:remember_token) { should_not be_blank }
   end
   end
+end
